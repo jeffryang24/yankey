d7901006fc3b52719f964381a772fc8a62d37103
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-30 10:37:52 +0700

    Initialization View _initVersion 0.0.1 _initDate 03-30-2015

---
+++ yw_apl/views/view_installer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?= $installer_page_title ?></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
