7af8b38b76b533bccc011ee16d188dbef11b2bef
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-30 12:21:01 +0700

    add form _initVersion 0.0.1 _initDate 03-30-2015

--- yw_apl/views/view_installer.php
+++ yw_apl/views/view_installer.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="description" content="Installer Page For Yankee-Whiskey">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?= base_url().'resources/css/installer.css' ?>">
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
@@ -21,10 +21,25 @@
                 <div class="col-md-8 col-md-offset-2" id="content">
                     <legend class="legend_s">Setup Progress</legend>
                     <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                            <b>60%</b>
+                        <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+                            <b>20%</b>
                         </div>
                     </div>
+                    <legend class="legend_s">Personal User Configuration</legend>
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="txtFirstName" class="col-sm-2 control-label">First Name</label>
+                            <div class="col-sm-4">
+                                <input type="text" id="txtFirstName" name="txtFirstName" class="form-control txtField" placeholder="first name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="txtLastName" class="col-sm-2 control-label">Last Name</label>
+                            <div class="col-sm-4">
+                                <input type="text" id="txtLastName" name="txtLastName" class="form-control txtField" placeholder="last name">
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
