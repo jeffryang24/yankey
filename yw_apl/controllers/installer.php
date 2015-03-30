5b98516aeb4c28e8ef5c40151245747bb70a6356
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-30 10:30:41 +0700

    initialization feature _initVersion 0.0.1 _initDate 03-30-2015

---
+++ yw_apl/controllers/installer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Description of Installer [Yankee-Whiskey CodeName: OperationZulu]
+ *
+ * @author Jeffry Angtoni
+ * @version 1.0.0b
+ * @revision none
+ * @date 03-30-2015
+ * @purpose Initialize first-time use for new user
+ * 
+ */
+class Installer extends CI_Controller{
+    
+    // index
+    public function index(){
+        
+    }
+}
