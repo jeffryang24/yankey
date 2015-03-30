34c0fcf253125bbdc0be96aba2d6f6cae38336dd
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-30 10:02:57 +0700

    Configuration _initVersion 0.0.1 _initAdded 03-30-2015

--- yw_apl/config/config.php
+++ yw_apl/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'ba9eQvbs6X6pD2yniYoolGL35u4LCUrc';
 
 /*
 |--------------------------------------------------------------------------
