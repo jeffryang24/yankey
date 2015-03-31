0480f387a53da74e29902dd3618ef1b293bebc11
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-31 18:35:18 +0700

    validation now use jquery not angularjs _initVersion 0.0.1 _initDate 03-31-2015

--- resources/js/installer.js
+++ resources/js/installer.js
@@ -7,11 +7,8 @@ var app = angular.module('instapp',[]);
 app.controller('instactrl',function($scope){
     //Initialize Progress Bar to 0%
     $scope.progress = 0;
-   
-    //Initialize page show/hide variable
+    
     // page1 default is 1 because it's the first page to show
     $scope.page1 = 1;
     $scope.page2 = $scope.page3 = $scope.page4 = 0;
-    
-    
 });
