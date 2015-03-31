732258ac9e8a55cc21e50dccc48fbde6ef21374a
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-31 18:39:27 +0700

    delete progress bar initialization _initVersion 0.0.1 _initDate 03-31-2015

--- resources/js/installer.js
+++ resources/js/installer.js
@@ -5,9 +5,6 @@
  *****/
 var app = angular.module('instapp',[]);
 app.controller('instactrl',function($scope){
-    //Initialize Progress Bar to 0%
-    $scope.progress = 0;
-    
     // page1 default is 1 because it's the first page to show
     $scope.page1 = 1;
     $scope.page2 = $scope.page3 = $scope.page4 = 0;
