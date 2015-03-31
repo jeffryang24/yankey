377692559d926702a0f3f4086693dddca20e6b76
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-31 17:37:53 +0700

    remove angular animation _initVersion 0.0.1 _initDate 03-31-2015

--- resources/js/installer.js
+++ resources/js/installer.js
@@ -3,7 +3,7 @@
  * @date 03-31-2015
  * @purpose Backend script for angularjs
  *****/
-var app = angular.module('instapp',['ngAnimate']);
+var app = angular.module('instapp',[]);
 app.controller('instactrl',function($scope){
     //Initialize Progress Bar to 0%
     $scope.progress = 0;
@@ -12,4 +12,6 @@ app.controller('instactrl',function($scope){
     // page1 default is 1 because it's the first page to show
     $scope.page1 = 1;
     $scope.page2 = $scope.page3 = $scope.page4 = 0;
+    
+    
 });
