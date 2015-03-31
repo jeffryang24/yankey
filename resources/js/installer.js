9154606a6d3a24e9b2042049e9531b3830e9d2a5
jeffryang24 <jeffryangtoni24@gmail.com>
2015-03-31 17:30:12 +0700

    remove animation and back to angular _initVersion 0.0.1 _initDate 03-31-2015

--- resources/js/installer.js
+++ resources/js/installer.js
@@ -5,19 +5,11 @@
  *****/
 var app = angular.module('instapp',['ngAnimate']);
 app.controller('instactrl',function($scope){
-   //Initialize Progress Bar to 0%
-   $scope.progress = 0;
+    //Initialize Progress Bar to 0%
+    $scope.progress = 0;
    
-   //Initialize page show/hide variable
-   // page1 default is 1 because it's the first page to show
-   $scope.page1 = 1;
-   $scope.page2 = $scope.page3 = $scope.page4 = 0;
-   
-   //Control Ptogress
-   $scope.controlProgress = function(obj){
-       console.log(obj);
-       if(obj.length == 0){
-           $scope.progress = ($scope.progress > 0) ? ($scope.progress - 20) : $scope.progress;
-       }
-   }
+    //Initialize page show/hide variable
+    // page1 default is 1 because it's the first page to show
+    $scope.page1 = 1;
+    $scope.page2 = $scope.page3 = $scope.page4 = 0;
 });
