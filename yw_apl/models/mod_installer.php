29540e49bb65ba81b32f3bc44c0f49523d07c99d
jeffryang24 <jeffryangtoni24@gmail.com>
2015-04-02 18:03:56 +0700

    Model for installer controller, this will insert data to database _initVersion 0.0.1 _initDate 04-02-2015

---
+++ yw_apl/models/mod_installer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of mod_installer
+ *
+ * @author Jeffry Angtoni
+ * @date 02-04-2015
+ * @purpose Installer Module
+ */
+class Mod_installer extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+    }
+}
