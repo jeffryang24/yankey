27cb18c45137f3648146a3157762ff96fae292e0
Jeffry Angtoni <jeffryang24@users.noreply.github.com>
2015-04-03 21:53:41 +0700

    added _insert method _initVersion 0.0.1 _initDate 04-03-2015

--- yw_apl/models/mod_installer.php
+++ yw_apl/models/mod_installer.php
@@ -7,7 +7,30 @@
  * @purpose Installer Module
  */
 class Mod_installer extends CI_Model {
+    
+    private $db;
+    
     public function __construct() {
         parent::__construct();
+        $this->db = $this->load->database();
     }
+    
+    /*
+     * @author      : Jeffry Angtoni
+     * @date        : April 3, 2015
+     * @purpose     : Insert data method for installer_controller
+     */
+    private function _insertData($first_name,$last_name,$username,$email,$password,$pin){
+        
+    }
+    
+    // Setter and Getter
+    public function __set($name, $value) {
+        $this->$name = $value;
+    }
+    
+    public function __get($name) {
+        return $this->$name;
+    }
+    
 }
